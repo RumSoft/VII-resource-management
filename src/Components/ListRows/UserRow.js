9b5e892b64dfbb0db275658f546e07eeccc83e91
Dominik Ruman <dr124@o2.pl>
2020-11-04 18:26:27 +0100

    fix after merge

--- src/Components/ListRows/UserRow.js
+++ src/Components/ListRows/UserRow.js
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
 import { Tooltip } from "@material-ui/core";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { Events, EventService } from "../../Services";
 import "./UserRow.scss";
 
 export default class UserRow extends Component {
@@ -15,10 +16,23 @@ export default class UserRow extends Component {
   render() {
     const { firstName, lastName, id } = this.props.user;
     return (
-      <div>
-        {firstName} {lastName} {emailAddress}
-        <button onClick={() => this.handleEditClick(id)}>Edytuj</button>
-        <button onClick={() => this.handleDeleteClick()}>Usuń</button>
+      <div className="list-row room-row">
+        <div className="list-row__content">
+          {firstName} {lastName}
+        </div>
+        <div className="list-row__actions">
+          <Link to={`/user/edit?userId=${id}`}>
+            <button className="list-row__actions__edit">E</button>
+          </Link>
+          <Tooltip title="Usuń">
+            <button
+              className="list-row__actions__delete"
+              onClick={() => this.handleDeleteClick()}
+            >
+              X
+            </button>
+          </Tooltip>
+        </div>
       </div>
     );
   }
--- src/Pages/EditUserPage/index.js
+++ src/Pages/EditUserPage/index.js
@@ -1,39 +1,53 @@
 import React, { Component } from "react";
 import { UserService, NotificationService } from "../../Services";
 import { Redirect } from "react-router-dom";
-import UserManager from "../../Components/UserManager"
+import UserManager from "../../Components/UserManager";
 
 export default class EditUserPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            user: null,
-            redirect: false
-        };
-    }
-
-    componentDidMount() {
-        let id = new URLSearchParams(window.location.search).get('userid');
-        UserService.getUser(id)
-            .then((res) => {
-                this.setState({ user: res.data });
-            });
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+      redirect: false,
     };
+  }
 
-    editUser(user) {
-        console.log(this.state.user)
-        UserService.editUser(user).then((e) => {
-            NotificationService.success(`Zmieniono dane użytkownika ${this.state.firstName} ${this.state.lastName} o adresie ${this.state.emailAddress} na ${user.firstName} ${user.lastName} o adresie ${user.emailAddress}`);
-            this.setState({ redirect: true });
-        }).catch((e) => {
-            NotificationService.apiError(e, "Edycja użytkownika nie powiodła się");
-        });
-    }
+  componentDidMount() {
+    let id = new URLSearchParams(window.location.search.toLowerCase()).get(
+      "userid"
+    );
+    console.log(id);
+    UserService.getUser(id).then((res) => {
+      this.setState({ user: res.data });
+    });
+  }
 
-    render() {
-        return <>
-            {this.state.redirect && <Redirect to="/dashboard" />}
-            {this.state.user && <UserManager onSave={(user) => this.editUser(user)} edit user={this.state.user} />}
-        </>
-    }
+  editUser(user) {
+    console.log(this.state.user);
+    UserService.editUser(user)
+      .then((e) => {
+        NotificationService.success(
+          `Zmieniono dane użytkownika ${this.state.firstName} ${this.state.lastName} o adresie ${this.state.emailAddress} na ${user.firstName} ${user.lastName} o adresie ${user.emailAddress}`
+        );
+        this.setState({ redirect: true });
+      })
+      .catch((e) => {
+        NotificationService.apiError(e, "Edycja użytkownika nie powiodła się");
+      });
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.redirect && <Redirect to="/dashboard" />}
+        {this.state.user && (
+          <UserManager
+            onSave={(user) => this.editUser(user)}
+            edit
+            user={this.state.user}
+          />
+        )}
+      </>
+    );
+  }
 }
