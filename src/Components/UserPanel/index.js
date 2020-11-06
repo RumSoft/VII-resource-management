e03198334f6fe92b091a4da83d58d6433594fa0a
san590 <46568905+san590@users.noreply.github.com>
2020-11-06 02:10:04 +0100

    ResourceRow added

--- src/Components/UserPanel/index.js
+++ src/Components/UserPanel/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
+import { ResourceRow } from "../ListRows";
 
 export default class UserPanel extends Component {
   render() {
-    return <div>Logged in as User.</div>;
+    return <div>Logged in as User.</div>
   }
 }
