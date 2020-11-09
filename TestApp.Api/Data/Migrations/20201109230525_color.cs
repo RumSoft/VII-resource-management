c7a1767f204d03c85346ad6ac4434d61360f7f45
Dominik Ruman <dr124@o2.pl>
2020-11-10 00:48:06 +0100

    x

---
+++ TestApp.Api/Data/Migrations/20201109230525_color.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TestApp.Api.Data.Migrations
+{
+    public partial class color : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Color",
+                table: "Rooms",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                table: "Attributes",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "Attributes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Color",
+                table: "Rooms",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
