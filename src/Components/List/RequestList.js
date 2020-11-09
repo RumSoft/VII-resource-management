999bdb82a6e90597a1f076b2211b7c649bd02914
san590 <46568905+san590@users.noreply.github.com>
2020-11-09 01:59:20 +0100

    Added RequestList

---
+++ src/Components/List/RequestList.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import { NotificationService, RequestService } from "../../Services";
+import { RequestRow } from "../ListRows";
+import EntityList from "./EntityList";
+import "./index.scss";
+
+export default class RequestList extends Component {
+    state = {
+        requests: [],
+    };
+    componentDidMount() {
+        this.fetchRequests();
+    }
+
+    fetchRequests() {
+        RequestService.getList().then((result) => {
+            const requests = result && result.data;
+            this.setState({ requests });
+        });
+    }
+
+    addRequestClick() {
+        let resourceId = prompt("Podaj id zasobu");
+        let takerId = prompt("Podaj id odbiorcy");
+        let quantity = prompt("Podaj ilość zasobu, którą chcesz przekazać")
+        if (!resourceId || !takerId || !quantity) return;
+
+        RequestService.addRequest(resourceId, takerId, quantity)
+            .then(() => {
+                NotificationService.success(`Dodano przekazanie zasobu o id "${resourceId}" w ilości "${quantity}" użytkownikowi o id "${takerId}"`);
+                this.fetchRequests();
+            })
+            .catch((e) => {
+                NotificationService.apiError(e, "Nie udało się dodać przekazania zasobu");
+            });
+    }
+
+    requestChanged(request, action) {
+        RequestService.editRequest(request.id, action)
+            .then(() => {
+                NotificationService.success(
+                    `Pomyślnie zmieniono tryb przekazania zasobu o id`,
+                    ` → ${request.id}`
+                );
+                /* stricte nic sie nie zmienia, wiec nie ma sensu albo ja jestem glupi
+                 this.setState({
+                   requests: this.state.requests.map((x) => {
+                     if (x.id === room.id) x.name = room.name;
+                     return x;
+                   }),
+                 }); */
+            })
+            .catch((e) => {
+                NotificationService.apiError(e, "Nie udało się zmienić trybu przekazania zasobu");
+            });
+    }
+
+    render() {
+        const { requests } = this.state;
+        return (
+            <EntityList
+                onReloadClick={() => this.fetchRequests()}
+                onAddClick={() => this.addRequestClick()}
+                entities={requests}
+                entityName="requests"
+                entityMapFunc={(x) => (
+                    <RoomRow
+                        onChange={(request) => this.requestChanged(request)}
+                        key={x.id}
+                        request={x}
+                    />
+                )}
+                title="Przekazania"
+            />
+        );
+    }
+}
--- src/Components/List/index.js
+++ src/Components/List/index.js
@@ -1,3 +1,4 @@
 export { default as AttributeList } from "./AttributeList";
+export { default as RequestList } from "./RequestList";
 export { default as RoomList } from "./RoomList";
 export { default as UserList } from "./UserList";
