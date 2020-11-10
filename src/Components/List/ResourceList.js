65a9b9e55a5d0d43825f4168e01b128276d21528
Dominik Ruman <dr124@o2.pl>
2020-11-11 00:41:24 +0100

    Update ResourceList.js

--- src/Components/List/ResourceList.js
+++ src/Components/List/ResourceList.js
@@ -5,58 +5,58 @@ import EntityList from "./EntityList";
 import "./index.scss";
 
 export default class ResourceList extends Component {
-    state = {
-        resources: [],
-    };
-    componentDidMount() {
-        this.fetchResources();
-    }
+  state = {
+    resources: [],
+  };
+  componentDidMount() {
+    this.fetchResources();
+  }
 
-    fetchResources() {
-        ResourceService.getList().then((result) => {
-            const resources = result && result.data;
-            this.setState({ resources });
+  fetchResources() {
+    ResourceService.getList().then((result) => {
+      const resources = result && result.data;
+      this.setState({ resources });
+    });
+  }
+
+  resourceDeleted(resource) {
+    ResourceService.deleteResource(resource.id)
+      .then(() => {
+        NotificationService.success(`Usunięto zasób ${resource.name}`);
+        this.setState({
+          resource: this.state.resource.filter((x) => x.id !== resource.id),
         });
-    }
+      })
+      .catch((e) => {
+        NotificationService.apiError(e, "Nie udało się usunąć zasobu");
+      });
+  }
 
-    resourceDeleted(resource) {
-        ResourceService.deleteResource(resource.id)
-            .then(() => {
-                NotificationService.success(`Usunięto zasób ${resource.name}`);
-                this.setState({
-                    resource: this.state.resource.filter((x) => x.id !== resource.id),
-                });
-            })
-            .catch((e) => {
-                NotificationService.apiError(e, "Nie udało się usunąć zasobu");
-            });
-    }
+  resourceChanged(resource) {
+    window.location = `/resource/edit?resourceId=${resource.id}`;
+  }
 
-    resourceChanged(resource) {
-        window.location = `/resource/edit?resourceId=${resource.id}`
-    }
+  render() {
+    const { resources } = this.state;
 
-    render() {
-        const { resources } = this.state;
-
-        return (
-            <EntityList
-                onReloadClick={() => this.fetchUsers()}
-                onAddClick={() => {
-                    window.location = "/resource/add";
-                }}
-                entities={resources}
-                entityName="resources"
-                entityMapFunc={(x) => (
-                    <ResourceRow
-                        onDelete={(resource) => this.resourceDeleted(resource)}
-                        onChange={(resource) => this.resourceChanged(resource)}
-                        key={x.id}
-                        resource={x}
-                    />
-                )}
-                title="Zasoby"
-            />
-        );
-    }
+    return (
+      <EntityList
+        onReloadClick={() => this.fetchResources()}
+        onAddClick={() => {
+          window.location = "/resource/add";
+        }}
+        entities={resources}
+        entityName="resources"
+        entityMapFunc={(x) => (
+          <ResourceRow
+            onDelete={(resource) => this.resourceDeleted(resource)}
+            onChange={(resource) => this.resourceChanged(resource)}
+            key={x.id}
+            resource={x}
+          />
+        )}
+        title="Zasoby"
+      />
+    );
+  }
 }
