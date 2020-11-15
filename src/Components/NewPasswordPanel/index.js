d69cf7bc1cdd250b0b0dde9c12b395c1df53d833
kryha5555 <kryha5555@gmail.com>
2020-11-15 20:36:56 +0100

    Update index.js

--- src/Components/NewPasswordPanel/index.js
+++ src/Components/NewPasswordPanel/index.js
@@ -67,6 +67,7 @@ export default class NewPasswordPanel extends Component {
             Nowe hasło
           </Card.Header>
         </Card.Content>
+
         <Card.Content>
           <Form onSubmit={(e) => this.handleSubmit(e)}>
             <Form.Input
@@ -94,8 +95,8 @@ export default class NewPasswordPanel extends Component {
               onClick={(e) => this.handleSubmit(e)}
             />
           </Form>
-
         </Card.Content>
+
         <Card.Content>
           <Button
             fluid
@@ -106,44 +107,7 @@ export default class NewPasswordPanel extends Component {
             Zaktualizuj hasło
                 </Button>
         </Card.Content>
-
       </Card >
-      // <div className="newpassword-form">
-      //   <form onSubmit={(e) => this.handleSubmit(e)}>
-      //     <input type="hidden" name="token" value={this.state.token} />
-
-      //     <h2 className="text-center">Nowe hasło</h2>
-      //     <div className="form-group">
-      //       Podaj nowe hasło
-      //       <input
-      //         type="password"
-      //         className="form-control"
-      //         name="password"
-      //         placeholder="Podaj hasło"
-      //         value={this.state.password}
-      //         onChange={(e) => this.handleChange(e)}
-      //       />
-      //     </div>
-
-      //     <div className="form-group">
-      //       Powtórz hasło
-      //       <input
-      //         type="password"
-      //         className="form-control"
-      //         name="repeatedPassword"
-      //         placeholder="Powtórz hasło"
-      //         value={this.state.repeatedPassword}
-      //         onChange={(e) => this.handleChange(e)}
-      //       />
-      //     </div>
-
-      //     <div className="form-group">
-      //       <button type="submit" className="btn btn-primary btn-block">
-      //         Zaktualizuj hasło
-      //       </button>
-      //     </div>
-      //   </form>
-      // </div>
     );
   }
 }
